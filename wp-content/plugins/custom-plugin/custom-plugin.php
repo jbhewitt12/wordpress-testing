ec1f60000ac4ed4ff41381a3c8c8d7b9b7dda094
jbhewitt12 <jbhewitt12@gmail.com>
2019-04-01 19:25:33 +1100

    blank plugin created

---
+++ wp-content/plugins/custom-plugin/custom-plugin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* @package CustomPlugin
+*/
+
+/*
+Plugin Name: Custom Plugin 
+Plugin URI: http://localhost/wordpress-testing/custom-plugin
+Description: A custom plugin for WordPress.
+Version: 1.0.0
+Author: Joshua Hewitt
+Author URI: https://github.com/jbhewitt12
+License: GPLv2 or later
+Text Domain: custom-plugin
+*/
