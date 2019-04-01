3ef399eed41ffcf5c81d51da925ea73f8947b7b2
jbhewitt12 <jbhewitt12@gmail.com>
2019-04-01 22:52:19 +1100

    basic plugin working after cleanup

--- wp-content/plugins/custom-plugin/custom-plugin.php
+++ wp-content/plugins/custom-plugin/custom-plugin.php
@@ -19,25 +19,17 @@ if (!defined('ABSPATH')){
 	die;
 }
 
-// WP Shortcode to display text on page or post.
-function wp_first_shortcode(){
+// WP Shortcode to display list of posts from external API.
+function custom_shortcode(){
 
-	$request = wp_remote_get( 'https://jsonplaceholder.typicode.com/posts?_limit=5' );
+	$request = wp_remote_get( 'https://jsonplaceholder.typicode.com/posts?_limit=10' );
 	if( is_wp_error( $request ) ) {
-		return false; // Bail early
+		return false; // end if something goes wrong
 	}
 	$body = wp_remote_retrieve_body( $request );
 	$data = json_decode( $body );
-	echo $body;
+	// echo $body;
 	if( ! empty( $data ) ) {
-	
-		// echo '<ul>';
-		// foreach( $data as $post ) {
-		// 	echo '<li>';
-		// 		echo '<p>' . $post->title . '</p>';
-		// 	echo '</li>';
-		// }
-		// echo '</ul>';
 
 		echo '<div class"="container">';
 		foreach( $data as $post ) {
@@ -46,33 +38,7 @@ function wp_first_shortcode(){
 		}
 		echo '</div>';
 
-
-
 	}
 
-	// $request = wp_remote_get( 'https://pippinsplugins.com/edd-api/products' );
-	// if( is_wp_error( $request ) ) {
-	// 	return false; // Bail early
-	// }
-	// $body = wp_remote_retrieve_body( $request );
-	// echo $body;
-	// $data = json_decode( $body );
-	// if( ! empty( $data ) ) {
-		
-	// 	echo '<ul>';
-	// 	foreach( $data->products as $product ) {
-	// 		echo '<li>';
-	// 			echo '<a href="' . esc_url( $product->info->link ) . '">' . $product->info->title . '</a>';
-	// 		echo '</li>';
-	// 	}
-	// 	echo '</ul>';
-	// }
 }
-add_shortcode("custom-plugin", "wp_first_shortcode");
-
-
-// class CustomPlugin {
-
-// }
-
-// $customPlugin = new CustomPlugin(); 
+add_shortcode("custom-plugin", "custom_shortcode");
